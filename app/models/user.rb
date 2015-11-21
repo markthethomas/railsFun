4ed9072cac2bfd1cce0d5863aeb9ec6c86e61799
Mark Thomas <markthethomas@gmail.com>
2015-11-20 20:50:48 -0800

    Create migration to add some more props to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  has_many :posts
+  has_many(:posts)
+
+  # Validation
+  validates(:username, presence: true, length: { maximum: 120, minimum: 4})
+
+  validates(:firstName, presence: true)
+  validates(:lastName, presence: true)
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118004846) do
+ActiveRecord::Schema.define(version: 20151121044644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20151118004846) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "gender"
+    t.string   "language"
+    t.string   "website"
   end
 
 end
